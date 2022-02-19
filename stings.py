0ddd17f4fb1d9119e49ed8134c5413b4de3eb275
k.p.vergis <k.p.vergis@gmail.com>
2022-02-19 11:50:38 +0200

    as

---
+++ stings.py
@@ -0,0 +1,20 @@
+#print('hello')
+keimeno="don't kill emotion"
+#print(keimeno)
+print(keimeno[::2])
+print(len(keimeno))
+#edw einai ena sxolio
+
+#print(keimeno.upper())
+keimeno=keimeno.upper()
+print(keimeno)
+keimeno=keimeno.lower()
+print(keimeno)
+
+keimlst=keimeno.split()
+print(keimlst)
+"""
+\n new line7
+\t just pressed tab
+
+"""
